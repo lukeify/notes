b2a590eac4af4c6dad395ab6c51f67f06c0d7a83
lukeify <5379845+lukeify@users.noreply.github.com>
2024-07-13 11:38:47 +1200

    ci(mdl): set span as an allowed element for MD033

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -9,6 +9,10 @@ rule 'MD026', :punctuation => '.,;:!'
 # Prefer correctly-ordered numbered lists for reading.
 rule 'MD029', :style => :ordered
 
+# Allow span elements within Markdown. We use these to mark words and phrases for no-spelling via the span[data-nospell]
+# selector.
+rule 'MD033', :allowed_elements => 'span'
+
 # Exclude problematic/buggy rules
 # See https://github.com/markdownlint/markdownlint/issues/489 for more details
 exclude_rule 'MD055'
