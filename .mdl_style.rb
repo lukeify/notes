8d17b53684ab49b04df344efb47270727a599cf2
Luke <5379845+lukeify@users.noreply.github.com>
2024-07-21 14:18:50 +1200

    ci: `mdl` adjustments (#13)
    * ci(mdl): adjust rules
    
    * chore: add trailing line to .editorconfig
    
    * fix(mdl): use dash unordered list style

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,7 +1,19 @@
 all
 
+# Prefer pound keys to start headers
+rule 'MD003', :style => :atx
+
+# Prefer dashes for unordered lists
+rule 'MD004', :style => :dash
+
+# Prefer four spaces for unordered list indentation
+rule 'MD007', :indent => 4
+
 # Line length is disabled as I write according to https://sembr.org
 exclude_rule 'MD013'
+
+# Allow headers to have the same content in different nesting contexts
+rule 'MD024', :allow_different_nesting => true
 
 # Allow trailing question marks in headers
 rule 'MD026', :punctuation => '.,;:!'
